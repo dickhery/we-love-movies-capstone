cee066f1ee7d4af1f7ab964ff457178fe8dc70fd
Richard Hery <dickhery@gmail.com>
2024-07-16 20:18:01 -0700

    Create reviews routes, controllers, and service

--- src/reviews/reviews.controller.js
+++ src/reviews/reviews.controller.js
@@ -2,40 +2,47 @@ const service = require("./reviews.service");
 const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
 const methodNotAllowed = require("../errors/methodNotAllowed");
 
-async function reviewExists(request, response, next) {
-  // TODO: Write your code here
-
-  next({ });
-}
-
-async function destroy(request, response) {
-  // TODO: Write your code here
-
-}
-
-async function list(request, response) {
-  // TODO: Write your code here
-
-  response.json({  });
-}
-
-function hasMovieIdInPath(request, response, next) {
-  if (request.params.movieId) {
+async function reviewExists(req, res, next) {
+  const review = await service.read(req.params.reviewId);
+  if (review) {
+    res.locals.review = review;
     return next();
   }
-  methodNotAllowed(request, response, next);
+  next({ status: 404, message: "Review cannot be found." });
 }
 
-function noMovieIdInPath(request, response, next) {
-  if (request.params.movieId) {
-    return methodNotAllowed(request, response, next);
+async function destroy(req, res) {
+  await service.destroy(res.locals.review.review_id);
+  res.sendStatus(204);
+}
+
+async function list(req, res) {
+  const data = await service.list(req.params.movieId);
+  res.json({ data });
+}
+
+function hasMovieIdInPath(req, res, next) {
+  if (req.params.movieId) {
+    return next();
+  }
+  methodNotAllowed(req, res, next);
+}
+
+function noMovieIdInPath(req, res, next) {
+  if (req.params.movieId) {
+    return methodNotAllowed(req, res, next);
   }
   next();
 }
 
-async function update(request, response) {
-  // TODO: Write your code here
-
+async function update(req, res) {
+  const updatedReview = {
+    ...res.locals.review,
+    ...req.body.data,
+    review_id: res.locals.review.review_id,
+  };
+  const data = await service.update(updatedReview);
+  res.json({ data });
 }
 
 module.exports = {
--- src/reviews/reviews.service.js
+++ src/reviews/reviews.service.js
@@ -3,18 +3,17 @@ const db = require("../db/connection");
 const tableName = "reviews";
 
 async function destroy(reviewId) {
-  // TODO: Write your code here
-  
+  return db(tableName).where({ review_id: reviewId }).del();
 }
 
 async function list(movie_id) {
-  // TODO: Write your code here
-  
+  return db(tableName)
+    .where({ movie_id })
+    .then((reviews) => Promise.all(reviews.map(setCritic)));
 }
 
 async function read(reviewId) {
-  // TODO: Write your code here
-  
+  return db(tableName).where({ review_id: reviewId }).first();
 }
 
 async function readCritic(critic_id) {
