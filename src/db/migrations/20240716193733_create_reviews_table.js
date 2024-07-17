23ad0a275026c3dbd55daf02883d39a442e1f00c
Richard Hery <dickhery@gmail.com>
2024-07-16 19:41:52 -0700

    Add migration files for movies, critics, reviews, theaters, and movies_theaters tables

--- src/db/migrations/20240716193733_create_reviews_table.js
+++ src/db/migrations/20240716193733_create_reviews_table.js
@@ -1,8 +1,18 @@
 
-exports.up = function(knex) {
+exports.up = function (knex) {
+    return knex.schema.createTable("reviews", (table) => {
+      table.increments("review_id").primary();
+      table.text("content");
+      table.integer("score");
+      table.timestamps(true, true);
+      table.integer("critic_id").unsigned().notNullable();
+      table.foreign("critic_id").references("critics.critic_id");
+      table.integer("movie_id").unsigned().notNullable();
+      table.foreign("movie_id").references("movies.movie_id");
+    });
+  };
   
-};
-
-exports.down = function(knex) {
+  exports.down = function (knex) {
+    return knex.schema.dropTable("reviews");
+  };
   
-};
--- src/db/migrations/20240716193802_create_movies_theaters_table.js
+++ src/db/migrations/20240716193802_create_movies_theaters_table.js
@@ -1,8 +1,16 @@
 
-exports.up = function(knex) {
+exports.up = function (knex) {
+    return knex.schema.createTable("movies_theaters", (table) => {
+      table.boolean("is_showing").defaultTo(false);
+      table.timestamps(true, true);
+      table.integer("movie_id").unsigned().notNullable();
+      table.foreign("movie_id").references("movies.movie_id");
+      table.integer("theater_id").unsigned().notNullable();
+      table.foreign("theater_id").references("theaters.theater_id");
+    });
+  };
   
-};
-
-exports.down = function(knex) {
+  exports.down = function (knex) {
+    return knex.schema.dropTable("movies_theaters");
+  };
   
-};
