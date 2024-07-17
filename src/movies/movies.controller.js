f98af84457c8d3b1433282527c388c8779a90c1e
Richard Hery <dickhery@gmail.com>
2024-07-16 19:59:47 -0700

    Create movies routes, controllers, and service

--- src/movies/movies.controller.js
+++ src/movies/movies.controller.js
@@ -1,23 +1,38 @@
 const service = require("./movies.service");
 const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
 
-async function movieExists(request, response, next) {
-  // TODO: Add your code here.
-
-  next({});
+async function movieExists(req, res, next) {
+  const movie = await service.read(req.params.movieId);
+  if (movie) {
+    res.locals.movie = movie;
+    return next();
+  }
+  next({ status: 404, message: "Movie cannot be found." });
 }
 
-async function read(request, response) {
-  // TODO: Add your code here
-  response.json({ data: "" });
+async function list(req, res) {
+  const { is_showing } = req.query;
+  const data = await service.list(is_showing);
+  res.json({ data });
 }
 
-async function list(request, response) {
-  // TODO: Add your code here.
-  response.json({ });
+async function read(req, res) {
+  res.json({ data: res.locals.movie });
+}
+
+async function listTheaters(req, res) {
+  const data = await service.listTheaters(req.params.movieId);
+  res.json({ data });
+}
+
+async function listReviews(req, res) {
+  const data = await service.listReviews(req.params.movieId);
+  res.json({ data });
 }
 
 module.exports = {
-  list: [asyncErrorBoundary(list)],
+  list: asyncErrorBoundary(list),
   read: [asyncErrorBoundary(movieExists), read],
+  listTheaters: [asyncErrorBoundary(movieExists), asyncErrorBoundary(listTheaters)],
+  listReviews: [asyncErrorBoundary(movieExists), asyncErrorBoundary(listReviews)],
 };
--- src/movies/movies.service.js
+++ src/movies/movies.service.js
@@ -6,11 +6,7 @@ async function list(is_showing) {
     .modify((queryBuilder) => {
       if (is_showing) {
         queryBuilder
-          .join(
-            "movies_theaters",
-            "movies.movie_id",
-            "movies_theaters.movie_id"
-          )
+          .join("movies_theaters", "movies.movie_id", "movies_theaters.movie_id")
           .where({ "movies_theaters.is_showing": true })
           .groupBy("movies.movie_id");
       }
@@ -18,11 +14,36 @@ async function list(is_showing) {
 }
 
 async function read(movie_id) {
-  // TODO: Add your code here
-  
+  return db("movies").select("*").where({ movie_id }).first();
+}
+
+async function listTheaters(movie_id) {
+  return db("theaters")
+    .join("movies_theaters", "theaters.theater_id", "movies_theaters.theater_id")
+    .select("theaters.*", "movies_theaters.is_showing", "movies_theaters.movie_id")
+    .where({ "movies_theaters.movie_id": movie_id });
+}
+
+async function listReviews(movie_id) {
+  return db("reviews")
+    .join("critics", "reviews.critic_id", "critics.critic_id")
+    .select("reviews.*", "critics.*")
+    .where({ "reviews.movie_id": movie_id })
+    .then((reviews) => reviews.map(setCritic));
+}
+
+async function readCritic(critic_id) {
+  return db("critics").where({ critic_id }).first();
+}
+
+async function setCritic(review) {
+  review.critic = await readCritic(review.critic_id);
+  return review;
 }
 
 module.exports = {
   list,
   read,
+  listTheaters,
+  listReviews,
 };
